015e0979e5c51ebb61d2677a6159be88cfedd894
Chad Lavimoniere <chad@chadlavimoniere.com>
2017-04-18 17:45:28 +0000

    some initial milligram changes

--- php/header.php
+++ php/header.php
@@ -7,7 +7,10 @@ echo '<!DOCTYPE html>
 <head> 
 <!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src= "https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f); })(window,document,"script","dataLayer","GTM-PHQGJWR");</script> <!-- End Google Tag Manager -->
   <script src="https://use.fontawesome.com/4cbdfa5ae9.js"></script>
-  <link rel="stylesheet" type="text/css" href="/style.css">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+<link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+<link rel="stylesheet" href="/styles/milligram.min.css">
+<link href="https://use.fontawesome.com/4cbdfa5ae9.css" media="all" rel="stylesheet">
   <meta name="description" content="' . $meta . '">
   <meta name="keywords" content="UX, User Experience Design, Web Design, prototyping">
   <meta name="author" content="Chad Lavimoniere">
