d605209d06ff82ae6e39984f2029157f71cfb131
Chad Lavimoniere <chad@chadlavimoniere.com>
2017-05-01 17:38:35 +0000

    use !empty in checking for listing images

--- php/list.php
+++ php/list.php
@@ -2,7 +2,7 @@
 echo '<li>
 <a href="/article/' . $row['id'] . '/' . seoUrl($row['name']) . '">' . $row['name'] . '</a>
 <div class="background">';
-if ($row['image_url'] != '') {
+if (!empty($row['image_url'])) {
     echo '<img src="' . $row['image_url'] . '">';
 }
 echo '</div></li>';
