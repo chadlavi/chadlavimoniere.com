5fd250b81927647cf47214440db15ff02b9118ec
Chad Lavimoniere <chad@chadlavimoniere.com>
2017-10-18 03:43:57 +0000

    add some more tests

---
+++ Test/html/404Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Test\html;
+
+class 404Test extends \PHPUnit_Framework_TestCase {
+    public function testTrueIsTrue() {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
---
+++ Test/html/searchTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Test\html;
+
+class searchTest extends \PHPUnit_Framework_TestCase {
+    public function testTrueIsTrue() {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
