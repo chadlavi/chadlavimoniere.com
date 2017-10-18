4d90cf952bb6e7a92b634a99bed7ed0f9b122b3b
Chad Lavimoniere <chad@chadlavimoniere.com>
2017-10-18 04:03:56 +0000

    seems travis does not like that I have a test called 404Test, renaming to return404Test

--- Test/html/404Test.php
+++ Test/html/404Test.php
@@ -2,7 +2,7 @@
 
 namespace Test\html;
 
-class 404Test extends \PHPUnit_Framework_TestCase {
+class return404Test extends \PHPUnit_Framework_TestCase {
     public function testTrueIsTrue() {
         $foo = true;
         $this->assertTrue($foo);
