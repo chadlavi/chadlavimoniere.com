fdeadbf6d66b459de62c6c74ef5efe7eae1aa0fe
Chad Lavimoniere <chad@chadlavimoniere.com>
2018-05-13 15:51:53 +0000

    stop sorting articles by updated

--- html/index.php
+++ html/index.php
@@ -2,12 +2,12 @@
 include '../php/gzip.php';
 include '../php/connect.php';
 $title = 'Chad Lavimoniere';
-$meta = 'Case studies in User Experience design';
-$keywords = 'UX, User Experience Design, Web Design, prototyping';
+$meta = 'Case studies in product design';
+$keywords = 'UX, User Experience Design, Product Design, Web Design, prototyping';
 include '../php/header.php';
 include '../php/seoURL.php';
 
-if ($result = $mysqli->query("SELECT id, name, image_url from article order by updated desc")) {
+if ($result = $mysqli->query("SELECT id, name, image_url from article order by created desc")) {
     echo'<body class="list">';
     include '../php/nav.php';
     echo '<div class="container">';
