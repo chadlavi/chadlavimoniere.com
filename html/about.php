2fc5fa1456d6d050a7b0313f654bd10682591525
Chad Lavimoniere <chad@chadlavimoniere.com>
2018-09-07 03:04:13 +0000

    about page changes

--- html/about.php
+++ html/about.php
@@ -31,9 +31,9 @@ echo '
             <a class="content is-block" target="_blank" href="https://github.com/chadlavi">https://github.com/chadlavi</a>
             <h2 class="heading">linkedin</h2>
             <a class="content is-block" target="_blank" href="https://www.linkedin.com/in/chadlavimoniere/">https://www.linkedin.com/in/chadlavimoniere/</a>
-            <h2 class="heading">portfolio</h2>
-            <p class="content is-block"><a target="_blank" href="/portfolio">http://chadlavimoniere.com/portfolio</a> (🔐*)</p>
-            <p class="is-size-7 content">* but the source code for this site is on github, so if you\'re clever, you can find it</p>
+            <h2 class="heading">portfolio 🔒*</h2>
+            <p class="content is-block"><a target="_blank" href="/portfolio">http://chadlavimoniere.com/portfolio</a> </p>
+            <p class="is-size-7 content">* password protected, but the source code for this site is on github, so if you\'re clever, you can find it</p>
         </article>
     </div>
     ';
